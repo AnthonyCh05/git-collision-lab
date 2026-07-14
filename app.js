16f49439380366aa69f75879a031ddd2ae0839ec
Fatch <anthonyfabian212@gmail.com>
2026-07-13 22:51:20 -0500

    fix: refactor metod calcularDescueto

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 function calcularDescuento(precio, porcentajeDescuento) {
-    let precioFinal = 0;
-
     const descuento = (precio * porcentajeDescuento) / 100;
-    precioFinal = precio - descuento;
-    return precioFinal;
+    return precio - descuento;
 }
