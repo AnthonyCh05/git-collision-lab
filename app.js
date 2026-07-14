badb05108cb7d2e73a1dbb104ea7f3ac6fe8ad99
Fatch <anthonyfabian212@gmail.com>
2026-07-13 22:54:21 -0500

    feat: refactor code

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 function calcularDescuento(precio, porcentajeDescuento) {
+    let precioFinal = 0;
+
     const descuento = (precio * porcentajeDescuento) / 100;
-    return precio - descuento;
+    precioFinal = precio - descuento;
+    return precioFinal;
 }
