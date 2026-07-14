3324e655770d41b63e2a2fa8a4ea7f9a137a1e18
AnthonyCh05 <anthonychangotasig.clearminds@gmail.com>
2026-07-14 03:54:44 +0000

    feat: add newPorcent

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 function calcularDescuento(precio, porcentajeDescuento) {
     const descuento = (precio * porcentajeDescuento) / 100;
-    return precio - descuento;
+    const newPorcent = 10
+    return (precio - descuento) * newPorcent;
 }
